9e434f02277b0d3e535f7bc518b1bfe3b574f92b
Olivia Steger <osteger@mit.edu>
2023-12-12 13:22:56 -0500

    Fix admin settings visibility

--- client/components/Setting/UpdateUserForm.vue
+++ client/components/Setting/UpdateUserForm.vue
@@ -68,24 +68,13 @@ const isUpdateCityDisabled = computed(() => {
   return !information.value.city;
 });
 function validate() {
-      validationErrors.value = [];
-      if (!cityModel) {
-        validationErrors.value.push('Please select a city');
-      }
-      // Other validation rules as needed
-    }
-
-async function updateUsername() {
-  await updateUser({ username: username.value });
-  await updateSession();
-  username.value = "";
+  validationErrors.value = [];
+  if (!cityModel.value) {
+    validationErrors.value.push("Please select a city");
+  }
+  // Other validation rules as needed
 }
 
-async function updatePassword() {
-  await updateUser({ password: password.value });
-  await updateSession();
-  password.value = "";
-}
 async function updateIncome() {
   information.value.city = currentCity.value;
   information.value.snapEligible = currentSnapEligible.value;
@@ -108,21 +97,19 @@ async function updateSnap() {
 }
 
 async function updateLocation() {
-  console.log('this is currentCity', currentCity, ' and this is new location ', cityModel)
+  console.log("this is currentCity", currentCity, " and this is new location ", cityModel);
   validate();
-      if (validationErrors.value.length === 0) {
-        information.value.city = cityModel.value;
-        information.value.snapEligible = currentSnapEligible.value;
-        information.value.annualIncome = currentAnnualIncome.value;
-        await fetchy("/api/users", "PATCH", { body: { update: { information: JSON.stringify(information.value) } } });
-        console.log(await fetchy(`/api/users/${currentUsername.value}`, "GET"));
-        const user = await fetchy(`/api/users/${currentUsername.value}`, "GET");
-        currentAnnualIncome.value = user.information.annualIncome;
-        currentCity.value = user.information.city;
-        currentSnapEligible.value = user.information.snapEligible;
-      }
-    
-  
+  if (validationErrors.value.length === 0) {
+    information.value.city = cityModel.value;
+    information.value.snapEligible = currentSnapEligible.value;
+    information.value.annualIncome = currentAnnualIncome.value;
+    await fetchy("/api/users", "PATCH", { body: { update: { information: JSON.stringify(information.value) } } });
+    console.log(await fetchy(`/api/users/${currentUsername.value}`, "GET"));
+    const user = await fetchy(`/api/users/${currentUsername.value}`, "GET");
+    currentAnnualIncome.value = user.information.annualIncome;
+    currentCity.value = user.information.city;
+    currentSnapEligible.value = user.information.snapEligible;
+  }
 }
 
 async function getInformation() {
@@ -141,87 +128,47 @@ async function getInformation() {
 onBeforeMount(async () => {
   await getInformation();
   await getCities();
-  console.log('this is cities', cities, 'and value', cities.value);
+  console.log("this is cities", cities, "and value", cities.value);
   loaded.value = true;
 });
 </script>
 
 <template>
-  <strong style="margin:16px;">UPDATE USER PROFILE</strong>
-  <div class="d-flex flex-column" id="back">
-    <v-form validate-on="submit lazy" @submit.prevent="updateUsername">
-      <v-text-field hide-details v-model="username" :placeholder="currentUsername" :persistent-placeholder="true" :rules="rules" label="Username"></v-text-field>
-
-      <v-btn type="submit" id="change" block class="mt-2" text="Change Username"></v-btn>
-    </v-form>
-    <br />
-    <v-divider class ="div"></v-divider>
-    <v-form validate-on="submit lazy" @submit.prevent="updatePassword">
-      <v-text-field hide-details v-model="password" label="Password" :rules="rules"></v-text-field>
-
-      <v-btn type="submit" id="change" block class="mt-2" text="Change Password"></v-btn>
-    </v-form>
-    <br />
-    <v-divider class ="div"></v-divider>
-    <br />
+  <v-card-item>
     <v-form validate-on="submit lazy" @submit.prevent="updateIncome">
       <v-text-field hide-details v-model="information.annualIncome" :placeholder="currentAnnualIncome" label="Current Annual Income" :persistent-placeholder="true" :rules="rules"></v-text-field>
 
       <v-btn type="submit" block id="change" class="mt-2" text="Change Income Amount"></v-btn>
     </v-form>
-    <br />
-    <v-divider class ="div"></v-divider>
-    <br />
+  </v-card-item>
+  <v-card-item>
     <v-form validate-on="submit lazy" @submit.prevent="updateLocation">
-      
-      <v-select v-model="cityModel" :hint="`Your currently located in ${currentCity}`" :items="cities" :error-messages="validationErrors" chips label="City"></v-select>
-      
-      
-    
-      <!-- <div class="pure-control-group">
-        <label for="aligned-location">Choose the city You Live In: </label>
-        <select v-model="information.city" id="aligned-location" required>
-          <option value="Current location" disabled>{{ currentCity }}</option>
-          <option v-for="city in cities" :key="city" :value="city">{{ city }}</option>
-        </select>
-      </div> -->
-      <!-- <v-select v-model="information.city" label="Select City" :placeholder="undefined" :persistent-placeholder="true" :items="cities" return-object></v-select> -->
+      <v-select v-model="cityModel" :hint="`Your location is currently set to ${currentCity}.`" :items="cities" :error-messages="validationErrors" chips label="City"></v-select>
       <v-btn hide-details type="submit" id="change" block class="mt-2" text="Change Location"></v-btn>
     </v-form>
-    <br />
-    <v-divider class ="div"></v-divider>
-    <br />
+  </v-card-item>
+  <v-card-item>
     <v-form validate-on="submit lazy" @submit.prevent="updateSnap">
-      <v-switch
-        color="primary"
-        inset
-        true-value="Yes"
-        false-value="No"
-        v-model="information.snapEligible"
-        hide-details
-        :label="`Do you recieve SNAP benefits/federal food assistance? ${currentSnapEligible}`"
-      ></v-switch>
+      <v-switch color="primary" inset true-value="Yes" false-value="No" v-model="information.snapEligible" hide-details :label="`Do you recieve SNAP benefits/federal food assistance?`"></v-switch>
       <v-btn type="submit" id="change" block class="mt-2" text="Change"></v-btn>
     </v-form>
-    <br />
-  </div>
+  </v-card-item>
 </template>
 <style>
-
 .form {
   padding-bottom: 0px;
 }
-#back{
+#back {
   border: 1px black;
-  padding:1em;
+  padding: 1em;
 }
 #change {
   background-color: var(--lighter-header);
-  color:white;
+  color: white;
 }
 #change:hover {
   background-color: white;
-  color:var(--lighter-header);
+  color: var(--lighter-header);
   border-color: var(--lighter-header);
   border-width: 2px;
 }
--- client/views/SettingView.vue
+++ client/views/SettingView.vue
@@ -2,10 +2,22 @@
 import router from "@/router";
 import { useUserStore } from "@/stores/user";
 import { storeToRefs } from "pinia";
+import { ref } from "vue";
 import UpdateUserForm from "../components/Setting/UpdateUserForm.vue";
 
 const { currentUsername, userType } = storeToRefs(useUserStore());
 const { logoutUser, deleteUser } = useUserStore();
+
+let username = ref("");
+let password = ref("");
+
+const rules = [
+  (value: any) => {
+    if (value) return "value";
+
+    return "You must enter a value.";
+  },
+];
 
 async function logout() {
   await logoutUser();
@@ -16,31 +28,45 @@ async function delete_() {
   await deleteUser();
   void router.push({ name: "Home" });
 }
+
+const { updateUser, updateSession } = useUserStore();
+
+async function updateUsername() {
+  await updateUser({ username: username.value });
+  await updateSession();
+  username.value = "";
+}
+
+async function updatePassword() {
+  await updateUser({ password: password.value });
+  await updateSession();
+  password.value = "";
+}
 </script>
 
 <template>
-  <main class="column">
-    <v-card elevation="3" class="panel">
-    <v-card-title><strong>Settings for {{ currentUsername }}</strong></v-card-title>
-    <v-card-actions>
-    <v-row class="m-0">
-      <v-col class="d-flex justify-end">
-        <v-btn class="button-error btn-small" @click="delete_">Delete Account</v-btn>
-        <v-btn class="btn-small" variant="flat" color="primary" @click="logout">Logout</v-btn>
-      </v-col>
-    </v-row>
-    
-    </v-card-actions>
-    <div v-if="userType === 'Client'">
-      <UpdateUserForm />
-    </div>
-    
+  <v-container class="mt-5 mb-5" style="margin-left: auto; margin-right: auto">
+    <v-card>
+      <v-card-title
+        ><strong>Settings for {{ currentUsername }}</strong></v-card-title
+      >
+      <v-card-item>
+        <v-form validate-on="submit lazy" @submit.prevent="updateUsername">
+          <v-text-field hide-details v-model="username" :placeholder="currentUsername" :persistent-placeholder="true" :rules="rules" label="Username"></v-text-field>
+          <v-btn type="submit" id="change" block class="mt-2" text="Change Username"></v-btn>
+        </v-form>
+      </v-card-item>
+      <v-card-item>
+        <v-form validate-on="submit lazy" @submit.prevent="updatePassword">
+          <v-text-field hide-details v-model="password" label="Password" :rules="rules"></v-text-field>
+          <v-btn type="submit" id="change" block class="mt-2" text="Change Password"></v-btn>
+        </v-form>
+      </v-card-item>
+      <UpdateUserForm v-if="userType === 'Client'" />
+      <v-card-actions>
+        <v-btn color="var(--red)" @click="delete_">Delete Account</v-btn>
+        <v-btn color="primary" @click="logout">Logout</v-btn>
+      </v-card-actions>
     </v-card>
-  </main>
+  </v-container>
 </template>
-<style>
-.panel{
-  margin-top: 1em;
-  width: 60vw;
-}
-</style>
